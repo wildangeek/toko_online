b9802205e5b0f2d953526b35b4dc46eabbf8ed57
wildangeek <54316377+wildangeek@users.noreply.github.com>
2020-06-12 13:43:11 +0700

    Update script.js.js

--- script.js.js
+++ script.js.js
@@ -1,4 +1,7 @@
 //Soal 1
+let pembeli = prompt('Nana kamu?')
+let yangDibeli = prompt('Udah beli produk apa?')
+
 function makasih(pembeli, yangDibeli){
     return `Terima kasih ${pembeli} telah membeli ${yangDibeli}`
 };
